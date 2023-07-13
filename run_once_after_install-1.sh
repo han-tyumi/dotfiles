bd894e6d4deec756d5e1dd282b9d98d46efd76dd
Matt Champagne <mmchamp95@gmail.com>
2023-07-13 13:55:00 -0600

    avoid reinstallation

--- run_once_after_install-1.sh
+++ run_once_after_install-1.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-set -x
 
 # Install Homebrew
 # https://brew.sh/
-echo "Installing Homebrew..."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  echo "Homebrew is already installed."
+else
+  echo "Installing Homebrew..."
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Install Nix
 # https://nixos.org/
-echo "Installing Nix..."
-bash -c "$(curl -L https://nixos.org/nix/install)"
+
+if [[ -x /run/current-system/sw/bin/nix ]]; then
+  echo "Nix is already installed."
+else
+  echo "Installing Nix..."
+  bash -c "$(curl -L https://nixos.org/nix/install)"
+fi
--- run_once_after_install-2.sh
+++ run_once_after_install-2.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-set -x
-
-# Backup shell configuration files
-sudo mv /etc/bashrc /etc/bashrc.backup-before-nix-darwin
-sudo mv /etc/zshrc /etc/zshrc.backup-before-nix-darwin
 
 # Add Home Manager channel
 # https://nix-community.github.io/home-manager/
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
+if nix-channel --list | grep -q "home-manager "; then
+  echo "Home Manager channel has already been added."
+else
+  echo "Adding Home Manager channel..."
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-channel --update
+fi
 
 # Install Nix Darwin
 # https://github.com/LnL7/nix-darwin
-nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
-./result/bin/darwin-installer
+if [[ -x /run/current-system/sw/bin/darwin-rebuild ]]; then
+  echo "Nix Darwin is already installed."
+else
+  # Backup shell configuration files
+  echo "Backing up shell configuration files..."
+  sudo mv /etc/bashrc /etc/bashrc.backup-before-nix-darwin
+  sudo mv /etc/zshrc /etc/zshrc.backup-before-nix-darwin
+
+  echo "Installing Nix Darwin..."
+  nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+  ./result/bin/darwin-installer
+fi
